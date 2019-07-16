c6b09bd9ea648d24d63c2873cd3646de8e89579a
Himanshu Shakhar <himanshushakhar@gmail.com>
2019-07-16 11:29:37 +0530

    convert gulp 3 to gulp 4

--- themes/custom/nfa/gulpfile.js
+++ themes/custom/nfa/gulpfile.js
@@ -1,124 +1,57 @@
-/*global -$ */
-'use strict';
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var sass = require('gulp-sass');
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
 
-// Error notifications
-var reportError = function(error) {
-  $.notify({
-    title: 'Gulp Task Error',
-    message: 'Check the console.'
-  }).write(error);
-  console.log(error.toString());
-  this.emit('end');
+
+// File paths
+const files = { 
+    scssPath: 'scss/**/*.scss',
+    jsPath: 'scripts/**/*.js'
 }
 
-// Sass processing
-gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss')
-      .pipe($.sourcemaps.init())
-      // Convert sass into css
-      .pipe($.sass({
-        outputStyle: 'nested', // libsass doesn't support expanded yet
-        precision: 10
-      }))
-      // Show errors
-      .on('error', reportError)
-      // Autoprefix properties
-      .pipe($.autoprefixer({
-        browsers: ['last 2 versions']
-      }))
-      // Write sourcemaps
-      // .pipe($.sourcemaps.write())
-      // Save css
-      .pipe(gulp.dest('css'))
-      .pipe(browserSync.reload({
-        stream: true
-      }));
-});
+// Sass task: compiles the style.scss file into style.css
+function scssTask(){    
+    return src(files.scssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('css')
+    ); // put final CSS in css folder
+}
 
-// process JS files and return the stream.
-gulp.task('js', function () {
-  return gulp.src('scripts/**/*.js')
-      .pipe(gulp.dest('scripts'));
-});
+// JS task: concatenates and uglifies JS files to theme.js
+function jsTask(){
+    return src([
+        files.jsPath
+        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+        ])
+        .pipe(concat('theme.js'))
+        .pipe(uglify())
+        .pipe(dest('js')
+    );
+}
 
-// Optimize Images
-gulp.task('images', function() {
-  return gulp.src('images/**/*')
-      .pipe($.imagemin({
-        progressive: true,
-        interlaced: true,
-        svgoPlugins: [{
-          cleanupIDs: false
-        }]
-      }))
-      .pipe(gulp.dest('images'));
-});
+// Watch task: watch SCSS and JS files for changes
+// If any change, run scss and js tasks simultaneously
+function watchTask(){
+    watch([files.scssPath, files.jsPath], 
+        parallel(scssTask, jsTask));    
+}
 
-// JS hint
-gulp.task('jshint', function() {
-  return gulp.src('scripts/*.js')
-      .pipe(reload({
-        stream: true,
-        once: true
-      }))
-      .pipe($.jshint())
-      .pipe($.jshint.reporter('jshint-stylish'))
-      .pipe($.notify({
-        title: "JS Hint",
-        message: "JS Hint says all is good.",
-        onLast: true
-      }));
-});
-
-// Beautify JS
-gulp.task('beautify', function() {
-  gulp.src('scripts/*.js')
-      .pipe($.beautify({indentSize: 2}))
-      .pipe(gulp.dest('scripts'))
-      .pipe($.notify({
-        title: "JS Beautified",
-        message: "JS files in the theme have been beautified.",
-        onLast: true
-      }));
-});
-
-// Compress JS
-gulp.task('compress', function() {
-  return gulp.src('scripts/*.js')
-      .pipe($.sourcemaps.init())
-      .pipe($.uglify())
-      .pipe($.sourcemaps.write())
-      .pipe(gulp.dest('scripts'))
-      .pipe($.notify({
-        title: "JS Minified",
-        message: "JS files in the theme have been minified.",
-        onLast: true
-      }));
-});
-
-
-// BrowserSync
-gulp.task('browser-sync', function() {
-  //watch files
-  var files = [
-    'scss/*.scss',
-    'images/**/*',
-    'templates/**/*.twig'
-  ];
-  //initialize browsersync
-  browserSync.init(files, {
-    proxy: "http://mysite.dev" // BrowserSync proxy, change to match your local environment
-  });
-});
-
-
-// Default task to be run with `gulp`
-gulp.task('default', ['sass', 'browser-sync'], function() {
-  gulp.watch("scss/**/*.scss", ['sass']);
-  gulp.watch("scripts/**/*.js", ['js']);
-});
+// Export the default Gulp task so it can be run
+// Runs the scss and js tasks simultaneously
+// then runs cacheBust, then watch task
+exports.default = series(
+    parallel(scssTask, jsTask), 
+    watchTask
+);
