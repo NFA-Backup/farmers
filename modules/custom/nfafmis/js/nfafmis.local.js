f50bc86a4baa87e6fbd481fb817e81feb9a3b6b5
Himanshu Shakhar <himanshushakhar@gmail.com>
2019-07-30 17:49:46 +0530

    show hide offer/licences tabs and content with js

--- modules/custom/nfafmis/js/nfafmis.local.js
+++ modules/custom/nfafmis/js/nfafmis.local.js
@@ -7,23 +7,47 @@
   Drupal.behaviors.nfamis = {
     attach: function (context, settings) {
 
-      // var row =  $('#66').parentsUntil("div.views-row").hide();
-      // Enable default tab.
-      // $('#tabs li a:not(:first)').addClass('inactive');
-      var row = $( ".view-id-offers_licences").children('div..views-row').hide();
-      console.log(row);
-      // $('.view-id-offers_licences.view-content .views-row').hide();
-      $('#66').parentsUntil("div.views-row").show();
+      /*
+            var row = $( ".view-id-offers_licences").children('div..views-row').hide();
+            console.log(row);
+            // $('.view-id-offers_licences.view-content .views-row').hide();
+            $('#66').parentsUntil("div.views-row").show();
 
-      $('#tabs li a').click(function () {
-        var t = $(this).attr('id');
-        if ($(this).hasClass('inactive')) {
-          $('#tabs li a').addClass('inactive');
-          $(this).removeClass('inactive');
-          $('.tabcontent').hide();
-          $('#data-' + t).fadeIn('slow');
-        }
+            $('#tabs li a').click(function () {
+              var t = $(this).attr('id');
+              if ($(this).hasClass('inactive')) {
+                $('#tabs li a').addClass('inactive');
+                $(this).removeClass('inactive');
+                $('.tabcontent').hide();
+                $('#data-' + t).fadeIn('slow');
+              }
+            });
+      */
+
+      $.fn.tabsMapper = function () {
+        $(this).find('div.views-row').each(function (i) {
+          $(this).find('a').click(function (e) {
+            e.preventDefault();
+
+            $(".offers-licences-wrapper").find('.views-row').hide();
+            $target = $(".offers-licences-wrapper").find('.elem-' + i);
+            $target.show();
+
+            $('html, body').animate({
+              scrollTop: $target.offset().top
+            }, 100);
+          });
+        });
+      }
+
+      $(".offers-licences-wrapper").find('div.views-row').each(function (i) {
+        $(this).addClass('elem-' + i);
+        $(this).hide();
       });
+
+      $(".offers-letter-dates-tabs").tabsMapper();
+      $(".licence-number-tabs").tabsMapper();
+
     }
   };
 }(jQuery));
