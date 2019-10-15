433e628e7c1b9db58e6a597985594b6e06322a70
Chandan <chandansingh7929@gmail.com>
2019-10-16 01:02:11 +0530

    Update module to create link block and config.

---
+++ modules/custom/nfafmis/src/Plugin/Block/NfafmisBlock.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\nfafmis\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+
+/**
+ * Provides a 'NFAMIS' Block.
+ *
+ * @Block(
+ *   id = "nfafmis_block",
+ *   admin_label = @Translation("NFAFMIS block"),
+ *   category = @Translation("NFAFMIS"),
+ * )
+ */
+class NfafmisBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $farmer_name = \Drupal::request()->query->get('title');
+    $options = [];
+    if ($farmer_name) {
+      $options = [
+        'query' => ['title' => $farmer_name]
+      ];
+    }
+
+    $current_path = \Drupal::service('path.current')->getPath();
+    $farmer_menu_items = [
+      ['uri' => 'internal:/tree-farmer-overview', 'label' => 'Farmer Log'],
+      ['uri' => 'internal:/tree-farmer-overview/details', 'label' => 'Farmer Details'],
+      ['uri' => 'internal:/tree-farmer-overview/licences', 'label' => 'Offers/Licenses'],
+      ['uri' => 'internal:/tree-farmer-overview/sub-areas', 'label' => 'Sub-areas'],
+      ['uri' => 'internal:/tree-farmer-overview/inventory', 'label' => 'Inventory'],
+      ['uri' => 'internal:/tree-farmer-overview/accounts', 'label' => 'Accounts'],
+      ['uri' => 'internal:/tree-farmer-overview/harvest', 'label' => 'Harvest'],
+    ];
+    foreach ($farmer_menu_items as $key => $value) {
+      // Set active class for the link.
+      $path_part = explode(":", $value['uri']);
+      if ($path_part[1] === $current_path) {
+        $options['attributes'] = [
+          'class' => [
+            'farmer-tabs active',
+          ],
+        ];
+      }else{
+        $options['attributes'] = [
+          'class' => [
+            'farmer-tabs',
+          ],
+        ];
+      }
+
+      $url = Url::fromUri($value['uri'], $options);
+      $link = Link::fromTextAndUrl($this->t($value['label']), $url);
+      $build[$key] = $link->toRenderable();
+    }
+    // Set cache contexts on query_args.
+    $build['#cache']['contexts'][] = 'url.query_args:title';
+
+    return $build;
+  }
+
+}
