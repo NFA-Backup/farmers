4f2f9061f91174394f05b5a8758584bc5cfbc677
Chandan <chandansingh7929@gmail.com>
2019-10-31 15:39:24 +0530

    Added modal on edit for offer/licenses.

--- modules/custom/nfafmis/js/nfafmis.local.js
+++ modules/custom/nfafmis/js/nfafmis.local.js
@@ -3,30 +3,22 @@
  * Contains local JS for showing/hiding license data.
  */
 
-(function ($) {
+(function($) {
   Drupal.behaviors.nfamis = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
 
-      /*
-            var row = $( ".view-id-offers_licences").children('div..views-row').hide();
-            console.log(row);
-            // $('.view-id-offers_licences.view-content .views-row').hide();
-            $('#66').parentsUntil("div.views-row").show();
+      $(context).find("td.views-field").once("nfamis").each(function() {
+        let editLink = $(this).find('a');
+        if (editLink.length) {
+          editLink.addClass('use-ajax');
+          editLink.attr("data-dialog-options", '{"width":800}');
+          editLink.attr("data-dialog-type", "modal");
+        }
+      });
 
-            $('#tabs li a').click(function () {
-              var t = $(this).attr('id');
-              if ($(this).hasClass('inactive')) {
-                $('#tabs li a').addClass('inactive');
-                $(this).removeClass('inactive');
-                $('.tabcontent').hide();
-                $('#data-' + t).fadeIn('slow');
-              }
-            });
-      */
-
-      $.fn.tabsMapper = function () {
-        $(this).find('div.views-row').each(function (i) {
-          $(this).find('a').click(function (e) {
+      $.fn.tabsMapper = function() {
+        $(this).find('div.views-row').each(function(i) {
+          $(this).find('a').click(function(e) {
             e.preventDefault();
 
             $(".offers-licences-wrapper").find('.views-row').hide();
@@ -40,7 +32,7 @@
         });
       }
 
-      $(".offers-licences-wrapper").find('div.views-row').each(function (i) {
+      $(".offers-licences-wrapper").find('div.views-row').each(function(i) {
         $(this).addClass('elem-' + i);
         $(this).hide();
       });
