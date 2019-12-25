62948c75508647a8fd973c32977d295207e2c6bd
Chandan Singh <chandansingh7929@gmail.com>
2019-12-25 17:58:18 +0530

    Update tab functionality for account tab.

--- modules/custom/nfafmis/js/nfafmis.local.js
+++ modules/custom/nfafmis/js/nfafmis.local.js
@@ -8,17 +8,46 @@
     attach: function(context, settings) {
 
       $(".account-tab-view", context).once("nfamis").each(function() {
-        var ulist = $("#account-sub-tabs").append('<ul class="nav nav-tabs farmer-tabs"></ul>').find('ul');
-         $(context).find(".account-list-tabs").each(function() {
-          console.log(this.children[0].innerText);
 
-          if (this.children[0].innerText) {
-            let name = this.children[0].innerText;
-            let filtered_name = name.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
-            let elem = '<a href="#'+filtered_name+'" class="tabs-item">'+name+'</a>';
-            ulist.append('<li class="tabs-item">'+elem+'</li>');
+        // Default hide all content, then show first as default.
+        // $('.account-tab-view .view-content .views-row .field-content .views-row').hide();
+
+        // Create tabs under account tab.
+        let ulist = $("#account-sub-tabs").append('<ul></ul>').find('ul');
+            ulist.addClass('nav nav-tabs farmer-tabs');
+
+        // Loop through existing li to create a new one and append them in ul.
+        $(context).find(".account-list-tabs").each(function(i,e) {
+          let anchorElem = $(this).find('a');
+
+          let name = anchorElem.text();
+          let filtered_name = name.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
+          let listElem = $('<li/>');
+          listElem.addClass('account-list-subtabs');
+          let tempElem = $('<a href />');
+          tempElem.addClass('tabs-item');
+          $(this).attr('id',filtered_name);
+          // tempElem.attr('style', 'margin-left: 50px;');
+          tempElem.attr('href', '#'+filtered_name);
+          tempElem.text(name);
+          tempElem.appendTo(listElem);
+          listElem.appendTo(ulist);
+          // Set active class for first tab by default.
+          if (i === 0) {
+            tempElem.addClass('active');
+            window.location = window.location+'#'+filtered_name;
+            console.log($(this).parent());
           }
-         });
+          // Bind click event for anchor tag.
+          $(tempElem).click(function(){
+            if (!$(this).hasClass('active')) {
+              let elemToHide = $(this).text().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
+              // console.log($('#'+elemToHide).parents('.views-row').show());
+              $('.account-list-subtabs a').removeClass('active');
+              $(this).addClass('active');
+            }
+          })
+        });
       });
 
       $(context).find("td.views-field").once("nfamis").each(function() {
