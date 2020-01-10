1f2d2d3038ab32ea82b40863adf89d603cab57c1
Chandan Singh <chandansingh7929@gmail.com>
2020-01-10 16:39:52 +0530

    Updated sub-area, inverntory tab to select list.

--- modules/custom/nfafmis/js/nfafmis.local.js
+++ modules/custom/nfafmis/js/nfafmis.local.js
@@ -11,20 +11,18 @@
 
         // A area ID filter on sub-area, inventory tab.
         let filterElem = '<div class="form-inline clearfix"><span class="control-label">Area ID:</span>';
-        filterElem+= '<div class="input-group"><input class="form-control ui-autocomplete-input" id="sub-area-filter"><span class="input-group-addon">';
-        filterElem+= '<span class="icon glyphicon glyphicon glyphicon-arrow-down"></span></span></div></div>';
+        filterElem+= '<div class="input-group"><select id="filter-sub-area" class="form-control ui-autocomplete-input"></select></div></div>';
         $(this).find('.center-container .area-filter-section').append(filterElem);
 
         // Add sub-area ID filter on inventroy tab.
         let filterSubElem = '<div class="form-inline clearfix"><span class="control-label">Sub-area ID:</span>';
-        filterSubElem+= '<div class="input-group"><input class="form-control ui-autocomplete-input" id="filter-sub-area"><span class="input-group-addon">';
-        filterSubElem+= '<span class="icon glyphicon glyphicon glyphicon-arrow-down"></span></span></div></div>';
+        filterSubElem+= '<div class="input-group"><select id="filter-sub-area" class="form-control ui-autocomplete-input"></select></div></div>';
         $(this).find('.center-container .area-filter-sub-area').append(filterSubElem);
 
 
-        // Event handler for down arrow.
-        $(this).find('span.input-group-addon').click(function() {
-          let filterVal = $('#sub-area-filter').val().trim();
+        // Event handler for area select list.
+        $(this).find('#filter-sub-area').change(function() {
+          let filterVal = $(this).children("option:selected").val();
           $(".sub-areas-planting .views-row").hide();
           $('.view-id-sub_areas_planting_status .views-row').hide();
           let filtered_name = filterVal.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
@@ -40,11 +38,14 @@
           let filtered_name = filterVal.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
           $(this).find('span').attr('id', filtered_name);
           let filterClass = $(this).find('span').attr('class');
+
+          // Show the first element value as default selected.
           if (index == 0) {
-            $('#sub-area-filter').val(filterVal);
             $(this).find('span').parents('.views-row').fadeIn('slow');
             $('.view-id-sub_areas_planting_status .views-row').find('span.'+filterClass).parents('.views-row').fadeIn('slow');
           }
+          // Append option in select list.
+          $('#filter-sub-area').append('<option value="'+filtered_name+'">'+filterVal+'</option>');
         });
       });
 
