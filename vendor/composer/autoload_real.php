921f6e60e315bd97757e5768566532a10fe13cad
miriam <miriam@peacog.com>
2021-05-27 09:14:00 +0200

    Drupal core and contrib module updates

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit2ad761d337188c0cdcd73927f2983911
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit2ad761d337188c0cdcd73927f2983911', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit2ad761d337188c0cdcd73927f2983911', 'loadClassLoader'));
@@ -34,7 +32,7 @@ class ComposerAutoloaderInit2ad761d337188c0cdcd73927f2983911
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit2ad761d337188c0cdcd73927f2983911::getInitializer($loader));
         } else {
