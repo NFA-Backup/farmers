a60f9754ebccb25f444a755973e758961c1a7b3d
miriam <miriam@cambrico.net>
2023-03-02 13:21:35 +0100

    updated mapbox key

--- docker/drupal/settings.overrides.php
+++ docker/drupal/settings.overrides.php
@@ -50,4 +50,4 @@ $settings['file_public_path'] = "sites/default/files";
 $settings['file_private_path'] = $docroot_base . '/private';
 $settings['file_temp_path'] = $docroot_base . '/tmp';
 
-$config['farm_map_mapbox.settings']['api_key'] = 'pk.eyJ1IjoibmF0LWZvci1hdXRoLXVnIiwiYSI6ImNsZGlvd255YjAydDUzbmxndDB4MzU5YnEifQ.u8Tb2PvtKSsxE0AsYJM-Qg';
+$config['farm_map_mapbox.settings']['api_key'] = 'pk.eyJ1IjoibmF0LWZvci1hdXRoLXVnIiwiYSI6ImNsZGlveTl0bDFqZDYzdm82bjBvdzRxN3EifQ.2TBv1-oxpDs9TY7s0seLGg';
