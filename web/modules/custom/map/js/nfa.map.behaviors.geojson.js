2b225dfa6efbe273861c8f2efa5133606f1d0dac
yash621 <yg17381@gmail.com>
2023-05-17 01:30:05 +0530

    #146 implemeted subarea highlight feature

--- web/modules/custom/map/js/nfa.map.behaviors.geojson.js
+++ web/modules/custom/map/js/nfa.map.behaviors.geojson.js
@@ -19,6 +19,51 @@
           instance.zoomToVectors()
         })
       }
+      const nodes = document.querySelectorAll('tbody')[2].querySelectorAll('tr')
+      const map = instance.map;
+      let previousNodeId = '';
+      nodes.forEach(function (node) {
+        node.style.cursor = 'pointer';
+        const nodeId = node.querySelectorAll('td')[1].innerText
+        node.setAttribute('id', nodeId);
+        node.addEventListener('click', function (event) {
+          const isSameNode = previousNodeId == nodeId;
+          node.style.backgroundColor = isSameNode ? '' : '#adebdb';
+          const alreadyAddedLayers = map.getLayers().getArray();
+          for (let i = 0; i < alreadyAddedLayers.length; i++) {
+            const node = document.querySelectorAll(".layer-switcher input")[i];
+            if (!isSameNode && node.checked) node.click()
+            if(isSameNode && !node.checked) node.click()
+          }
+          if (previousNodeId != '') { 
+            map.removeLayer(alreadyAddedLayers[alreadyAddedLayers.length - 1])
+            const previousSelectedNode = document.getElementById(`${previousNodeId}`);
+            previousSelectedNode.style.backgroundColor = '';
+          }
+          if (isSameNode) {
+            const layerSwitcher = document.querySelector('.layer-switcher')
+            layerSwitcher.style.display = "block";
+            instance.zoomToVectors()
+            previousNodeId = '';
+            return;
+          }
+          fetch(`${window.origin}/sub-area/geojson/${nodeId}`).then(async (response) => {
+            const geoJson = await response.json()
+            console.log(geoJson)
+            const layer = instance.addLayer('geojson', {
+              title: `${nodeId} id subarea`,
+              geojson : geoJson,
+              color: 'orange',
+            });
+            const source = layer.getSource()
+            previousNodeId = nodeId;
+            const layerSwitcher = document.querySelector('.layer-switcher')
+            layerSwitcher.style.display = "none"
+            map.getView().fit(source.getExtent());
+            map.getView().fit(geoJson, map.getSize(), {duration: 1000});
+          })
+        })
+      })
     }
   }
 }())
