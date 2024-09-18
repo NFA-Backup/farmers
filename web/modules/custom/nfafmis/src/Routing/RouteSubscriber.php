c287bdec007fbeacaee9ab41a5d50e3155f4c29d
miriam <miriam@cambrico.net>
2024-09-18 13:58:43 +0200

    Issue #209: configure json api

--- web/modules/custom/nfafmis/src/Routing/RouteSubscriber.php
+++ web/modules/custom/nfafmis/src/Routing/RouteSubscriber.php
@@ -19,6 +19,20 @@ class RouteSubscriber extends RouteSubscriberBase {
     if ($route = $collection->get('entity.taxonomy_term.canonical')) {
       $route->setDefault('_controller', '\Drupal\nfafmis\Controller\TaxonomyTermViewPageController::handle');
     }
+    foreach ($collection as $name => $route) {
+      // Alter JSON:API routes to check for 'access jsonapi routes' permission.
+      $defaults = $route->getDefaults();
+      if (!empty($defaults['_is_jsonapi']) && !empty($defaults['resource_type'])) {
+        $route->setRequirement('_permission', 'access jsonapi routes');
+
+        // Remove DELETE and POST methods from JSON:API routes.
+        $methods = $route->getMethods();
+        if (in_array('DELETE', $methods) || in_array('POST', $methods)) {
+          // We never want to delete or post data.
+          $collection->remove($name);
+        }
+      }
+    }
   }
 
   /**
